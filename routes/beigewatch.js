5942cd15da04ed1bb9a4975825eb038f648ab856
Duncan Angus Wilkie <duncanwilkie@hotmail.com>
2012-06-07 20:17:22 +0100

    Shorted require() statement in beigewatch + comments

--- routes/beigewatch.js
+++ routes/beigewatch.js
@@ -1,4 +1,5 @@
-var request = require('../node_modules/request/'),
+var request = require('request'),
+//var request = require('../node_modules/request/'), -- Not necessary as node_modules is the default for require.
     url = 'http://search.twitter.com/search.json?q=',
     username = 'bristolwebfolk';
 
@@ -6,31 +7,31 @@ exports.index = function (req, res) {
   request.get({json: true, url: url + '@' + username}, function (error, response, body) {
     if (!error && response.statusCode == 200) {
 
-        var results = body.results,
-            len = results.length,
-            i;
+      var results = body.results,
+          len = results.length,
+          i;
 
-        if (len) {
-          for (i = 0; i < len; i += 1) {
-            /*
-            if (results[i].text.match(/^RT /)) {
-              results.splice(i, 1);
-              len -= len;
-              i -= 1;
-              continue;
-            }*/
+      if (len) {
+        for (i = 0; i < len; i += 1) {
+          /*
+          if (results[i].text.match(/^RT /)) {
+            results.splice(i, 1);
+            len -= len;
+            i -= 1;
+            continue;
+          }*/
 
-            if (results[i].text.match(/https?:\/\//)) {
-              results[i].text = results[i].text.replace(/(https?:\/\/[^\s]+)/gi, '<a href="$1">$1</a>');
-              console.log(results[i].text);
-            }
+          if (results[i].text.match(/https?:\/\//)) {
+            results[i].text = results[i].text.replace(/(https?:\/\/[^\s]+)/gi, '<a href="$1">$1</a>');
+            console.log(results[i].text);
           }
         }
-
-        res.render('beigewatch', {
-          title: 'Beige Watch',
-          tweets: body.results
-        });
       }
-    });
+
+      res.render('beigewatch', {
+        title: 'Beige Watch',
+        tweets: body.results
+      });
+    }
+  });
 };
